c806c3ff5899d10f2ff609fb3806ad53d646eaed
CorentinPeutin <corentin.peutin@ensg.eu>
2023-07-20 11:28:48 +0200

    Correction de test_source suite aux modifications de la classe source

--- tests/test_source.py
+++ tests/test_source.py
@@ -41,6 +41,9 @@ def test_init_sourcepyramids(mocked_pyramid_class):
     pyramid_instance.tms = tms_instance
     pyramid_instance.descriptor = "s3://pyramids/SCAN1000.json"
     pyramid_instance.get_level.return_value = level_instance
+    pyramid_instance.storage_s3_cluster = None
+    pyramid_instance.type = PyramidType.RASTER
+    pyramid_instance.channels = 3
 
     tms_instance2 = MagicMock()
     tms_instance2.srs = "EPSG:3857"
@@ -64,6 +67,9 @@ def test_init_sourcepyramids(mocked_pyramid_class):
     pyramid_instance2.tms = tms_instance2
     pyramid_instance2.descriptor = "s3://pyramids/SCAN1000.json"
     pyramid_instance2.get_level.return_value = level_instance2
+    pyramid_instance2.storage_s3_cluster = None
+    pyramid_instance2.type = PyramidType.RASTER
+    pyramid_instance2.channels = 3
     mocked_pyramid_class.side_effect = [pyramid_instance, pyramid_instance2]
 
     try:
@@ -79,67 +85,5 @@ def test_init_sourcepyramids(mocked_pyramid_class):
         mocked_pyramid_class.assert_has_calls(
             [call("s3://pyramids/SCAN1000.json"), call("s3://pyramids/SCAN2000.json")]
         )
-    except Exception as exc:
-        assert False, f"Source pyramids creation raises an exception: {exc}"
-
-
-@mock.patch("rok4.layer.Pyramid.from_descriptor")
-def test_init_sourcerasterpyramids(mocked_pyramid_class):
-    tms_instance = MagicMock()
-    tms_instance.srs = "EPSG:3857"
-    tms_instance.name = "TMS"
-
-    level_instance = MagicMock()
-    level_instance.id = "10"
-    level_instance.resolution = 1
-    level_instance.tile_limits = {"min_row": 10, "max_row": 20, "min_col": 5, "max_col": 10}
-    level_instance.slab_width = 16
-    level_instance.slab_height = 16
-
-    pyramid_instance = MagicMock()
-    pyramid_instance.raster_specifications = {
-        "channels": 3,
-        "nodata": "255,255,255",
-        "photometric": "rgb",
-        "interpolation": "bicubic",
-    }
-    pyramid_instance.format = "TIFF_JPG_UINT8"
-    pyramid_instance.tms = tms_instance
-    pyramid_instance.descriptor = "s3://pyramids/SCAN1000.json"
-    pyramid_instance.type = PyramidType.RASTER
-    pyramid_instance.channels = 3
-    pyramid_instance.get_level.return_value = level_instance
-
-    tms_instance2 = MagicMock()
-    tms_instance2.srs = "EPSG:3857"
-    tms_instance2.name = "TMS"
-
-    level_instance2 = MagicMock()
-    level_instance2.id = "10"
-    level_instance2.resolution = 1
-    level_instance2.tile_limits = {"min_row": 2, "max_row": 12, "min_col": 8, "max_col": 20}
-    level_instance2.slab_width = 16
-    level_instance2.slab_height = 16
-
-    pyramid_instance2 = MagicMock()
-    pyramid_instance2.raster_specifications = {
-        "channels": 3,
-        "nodata": "255,255,255",
-        "photometric": "rgb",
-        "interpolation": "bicubic",
-    }
-    pyramid_instance2.format = "TIFF_JPG_UINT8"
-    pyramid_instance2.tms = tms_instance2
-    pyramid_instance2.descriptor = "s3://pyramids/SCAN1000.json"
-    pyramid_instance2.type = PyramidType.RASTER
-    pyramid_instance2.channels = 3
-    pyramid_instance2.get_level.return_value = level_instance2
-    mocked_pyramid_class.side_effect = [pyramid_instance, pyramid_instance2]
-
-    try:
-        datasources = SourceRasterPyramids(
-            "10", "10", ["s3://pyramids/SCAN1000.json", "s3://pyramids/SCAN2000.json"]
-        )
-        assert datasources.channels == 3
     except Exception as exc:
         assert False, f"Source pyramids creation raises an exception: {exc}"
